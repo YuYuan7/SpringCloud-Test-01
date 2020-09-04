13cb4d93516123d4399982f46753352c2df08ec7
yuanyu_2013@163.com <yuanyu_2013@163.com>
2020-09-04 11:01:51 +0800

    提交代码

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/myrule/RandomRule_YY.java
@@ -0,0 +1,88 @@
+package com.atguigu.myrule;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AbstractLoadBalancerRule;
+import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.Server;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomRule_YY extends AbstractLoadBalancerRule {
+
+    private int count = -1;
+    private int pos = 0;
+
+    public Server choose(ILoadBalancer lb, Object key) {
+        if (lb == null) {
+            return null;
+        }
+        Server server = null;
+
+        while (server == null) {
+            if (Thread.interrupted()) {
+                return null;
+            }
+            List<Server> upList = lb.getReachableServers();
+            List<Server> allList = lb.getAllServers();
+
+            int serverCount = allList.size();
+            if (serverCount == 0) {
+                /*
+                 * No servers. End regardless of pass, because subsequent passes
+                 * only get more restrictive.
+                 */
+                return null;
+            }
+
+            if(pos >= 5){
+                int index = chooseRandomInt(serverCount);
+                count = index;
+                pos = 0;
+            }else{
+                if(count == -1){
+                    int index = chooseRandomInt(serverCount);
+                    count = index;
+                }
+            }
+            pos++;
+            server = upList.get(count);
+
+            System.out.println(" *****>>>>>****** " + server.getPort());
+
+            if (server == null) {
+                /*
+                 * The only time this should happen is if the server list were
+                 * somehow trimmed. This is a transient condition. Retry after
+                 * yielding.
+                 */
+                Thread.yield();
+                continue;
+            }
+
+            if (server.isAlive()) {
+                return (server);
+            }
+
+            // Shouldn't actually happen.. but must be transient or a bug.
+            server = null;
+            Thread.yield();
+        }
+
+        return server;
+
+    }
+
+    protected int chooseRandomInt(int serverCount) {
+        return ThreadLocalRandom.current().nextInt(serverCount);
+    }
+
+    @Override
+    public Server choose(Object key) {
+        return choose(getLoadBalancer(), key);
+    }
+
+    @Override
+    public void initWithNiwsConfig(IClientConfig iClientConfig) {
+
+    }
+}
